45a7b166eb81bef7b8cf6952e3e43319edc4cf18
Latif1218 <mdsabdullotif@gmail.com>
2026-02-13 17:00:41 +0600

    update onboading_model.py

--- app/models/onboarding_model.py
+++ app/models/onboarding_model.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Float, Date, JSON, Boolean, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from ..database import Base
-from datetime import datetime
+
 
 
 class Onboarding(Base):
